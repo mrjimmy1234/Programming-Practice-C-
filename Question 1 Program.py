b77cd5abdd1b3997a075598ccc4a929023305d65
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-07-13 11:30:40 +0100

    Add files via upload

---
+++ Question 1 Program.py
@@ -0,0 +1,31 @@
+"""
+Author: Jimmy Arnott
+Date: 13/07/2016
+Purpose: Question 1
+"""
+
+X = input("Please enter the length X")
+Y = input("Please enter the length Y")
+Z = input("Please enter the angle Z")
+
+if X == "90":
+    if X == Y:
+        print("SQUARE")
+        exit()
+    else:
+        print("RECTANGLAR")
+        exit()
+
+else:
+    if A == "60" or "120":
+        print("HEXAGONAL")
+        exit()
+
+    else:
+        if X == Y:
+            print("RHOMBIC")
+            exit()
+        else:
+            print("PARALLELOGRAM")
+            exit()
+    
