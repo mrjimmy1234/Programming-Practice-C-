5c1eacc360ed5a15a792d272a759d3a74944acaf
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-19 11:41:48 +0100

    Add files via upload

---
+++ Part 4 - Strings in C Sharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_4___Strings_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = @"C:\DTMRKIV\User\Documents\C#_Directory
+            Console.WriteLine(Name);
+
+        }
+    }
+}
