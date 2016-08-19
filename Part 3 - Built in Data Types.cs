a7a7a206b3ccbda6d1e5134695efcd6c918224da
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-19 11:27:45 +0100

    Add files via upload

---
+++ Part 3 - Built in Data Types.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3___Built_in_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Integer
+            int i = 0;
+            Console.WriteLine("Min = {0}", int.MinValue);
+            Console.WriteLine("Max = {0}", int.MaxValue);
+
+            //Double
+            double d = 123.22242433;
+            Console.WriteLine(d);
+        }
+    }
+}
