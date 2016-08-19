d4cb77af86803874486e3e0f164f0be18389891f
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-19 09:39:53 +0100

    Add files via upload

---
+++ Part 2 - Introduction to C sharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2___Introduction_to_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, {1}", FirstName, LastName);
+        
+            //Console.WriteLine("Hello " + UserName);
+
+
+        }
+    }
+}
