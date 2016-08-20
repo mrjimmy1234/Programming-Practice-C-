c669a3dd79755a525fde62943be25cb168043874
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-20 11:08:21 +0100

    Add files via upload

---
+++ Part 9 - Comments in C Sharp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_9___Comments_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Single line comments //
+            // Multi line comment /* */
+            // XML documentation comments ///
+
+            //Console.WriteLine("Hello");
+
+            /*
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+            */
+
+            //Keyboard shortcuts - Ctrl C + Ctrl K to comment
+            // Ctrl C + Ctrl U to uncomment
+        }
+    }
+}
