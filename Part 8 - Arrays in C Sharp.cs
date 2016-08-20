adaeb93b0480f20ffe5cbfc5aee29db64859ef11
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-20 11:00:38 +0100

    Add files via upload

---
+++ Part 8 - Arrays in C Sharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_8___Arrays_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+            int[] Even_Numbers = new int[3];
+            Even_Numbers[0] = 0;
+            Even_Numbers[1] = 2;
+            Even_Numbers[2] = 4;
+            Console.WriteLine(Even_Numbers[1]);
+
+
+        }
+    }
+}
