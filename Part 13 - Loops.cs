9fe2d548ecafe3d2f1acb0891b5a04c81c929add
mrjimmy1234 <jimmy.arnott@gmail.com>
2016-08-21 11:05:34 +0100

    Add files via upload

---
+++ Part 13 - Loops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_13___Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your target");
+            int User_Target = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+            while(Start <= User_Target)
+            {
+                Console.WriteLine(Start + " ");
+                Start = Start + 2;
+            }
+        }
+    }
+}
